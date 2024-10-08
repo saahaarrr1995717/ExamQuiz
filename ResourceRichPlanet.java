f2a3eeaeea0fb6d87e232489c031bce802dea65a
ASUS <hoseinkhalafi74@gmail.com>
2024-10-08 16:46:49 +0330

    java exam

---
+++ ResourceRichPlanet.java
@@ -0,0 +1,22 @@
+import src.main.java.Planet;
+import src.main.java.PlanetType;
+
+import java.util.ArrayList;
+
+public class ResourceRichPlanet extends Planet {
+    private ArrayList<String> resources;
+
+    public ResourceRichPlanet(String name, PlanetType type, int numberOfMoons, double distanceFromSun, ArrayList<String> resources) {
+        super(name, type, numberOfMoons, distanceFromSun);
+        this.resources = resources;
+    }
+
+    public ArrayList<String> getResources() {
+        return resources;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", منابع طبیعی: " + String.join(", ", resources);
+    }
+}
---
+++ src/main/java/PlanetType.java
@@ -0,0 +1,17 @@
+package src.main.java;
+
+public enum PlanetType {
+    ROCK("سنگی"),
+    GAS("گازی");
+
+    private final String persianName;
+
+    PlanetType(String persianName) {
+        this.persianName = persianName;
+    }
+
+    @Override
+    public String toString() {
+        return persianName;
+    }
+}
